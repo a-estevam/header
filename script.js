443abf7f720f93c20087f4f1f8c03b9f8d69b5d0
a-estevam <estevam.adesign@gmail.com>
2022-09-15 23:22:31 -0300

    Menu scroll e mobile

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-window.addEventListener("scroll",function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+//menuMobile//
+
+const hamburger = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navmenu.classList.toggle("active");
+    
 })
 
+//scroll//
+
 window.addEventListener("scroll",function(){
-    var logo = document.querySelector("logo");
-    logo.classList.toggle("sticky", window.scrollY > 0);
-})
+        var header = document.querySelector("header");
+        header.classList.toggle("sticky", window.scrollY > 0);
+    })
+    
+    window.addEventListener("scroll",function(){
+        var logo = document.querySelector("logo");
+        logo.classList.toggle("sticky", window.scrollY > 0);
+    })
+    
+    
